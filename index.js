1bc3b2f13f8a31aedf6a1b135de38145353cacff
Pixellgame <74014567+Pixellgame@users.noreply.github.com>
2022-07-12 15:22:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+function onstart(){
+    document.getElementById('status').innerHTML = '';
+    document.getElementById('location').innerHTML = '';
+}
+
+var city =""
+
+var stop = false;
+
+function set(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
+var random=set(100,500);
+
+async function sendip(ev){
+    ev.preventDefault();
+    const IP = document
+    .getElementById('ip').value;
+
+    onstart();
+
+    fetch('https://extreme-ip-lookup.com/json/'+IP+'?key=upNQnug6yt6l5Dhpswks')
+    .then( res => res.json())
+    .then(response => {
+        console.log("Country: ", response.city);
+        city = response.city
+    })
+    .catch((data, status) => {
+        console.log('Request failed');
+        stop = true;
+    })
+
+    if(stop == false){
+        document.getElementById('location').innerHTML = 'Loading...';
+
+        setTimeout(function(){
+            document.getElementById('loading').style.opacity = '1';
+            document.getElementById('location').innerHTML = 'Location : '+ city;
+            move();
+        },5000);
+
+        setTimeout(function(){
+            document.getElementById('loading').style.opacity = '0';
+            document.getElementById('status').innerHTML = 'Success !';
+        },15500)
+
+        setTimeout(function(){
+            window.open('https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000030939443/','_blank');
+            var audio = new Audio('./sound.mp3');
+            audio.play();
+            document.body.style.backgroundImage = "url('https://media.makeameme.org/created/youve-been-trolled-k5g4hx.jpg')";
+        },17000)
+    }
+    else{
+        document.getElementById('status').innerHTML = 'Request failed !';
+    }
+
+
+};
+
+function move() {
+    var elem = document.getElementById("myBar");   
+    var width = 1;
+    var id = setInterval(frame, random);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+}
